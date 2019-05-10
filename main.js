e4c117f225ec9c3f12170d8726dda97bcdb76b04
Riley Shaw <rileyjshaw@gmail.com>
2019-05-10 15:03:53 -0400

    v1

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 !function() {
 	WebFont.load({
-		google: { families: ['Poppins:400,600'] },
+		google: {
+			families: [
+				'Poppins:400,600',
+				'Amatic SC',
+			],
+		},
 		active: function () {
-			fitterHappierText(document.querySelectorAll('.stretch'));
+			fitterHappierText(document.querySelectorAll('.stretch'), {baseline: '90%', paddingY: -6});
+			document.body.classList.remove('hide');
 		},
 	});
 }();
